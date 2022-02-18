7f78827b946a75b155112c969532bc7eb3d6145b
Alexander Ciaciek <ciaciek.polska@gmail.com>
2022-02-17 22:09:54 -0500

    Fin CACC

--- TP2/criteres.py
+++ TP2/criteres.py
@@ -1,22 +1,10 @@
-# def createTruthTable():
-#     counter = 0
-#     table = {}
-#     for p in [0, 1]:
-#         for h in [0, 1]:
-#             for u in [0, 1]:
-#                 for g in [0, 1]:
-#                     table[counter] = [p,h,u,g, verifyIsSpam(p,h,u,g)]
-#                     counter += 1
-#     return table
-
 def verifyIsSpam(p, h, u, g):
     return p and ((h and u) or (u and not g))
-
 
 def verifyIsSpamDNF(p, h, u, g):
     return p and h and u or u and not g
 
-def createTruthTable():
+def getTable():
     counter = 0
     table = {}
     for p in [False, True]:
@@ -27,52 +15,93 @@ def createTruthTable():
                     counter += 1
     return table
 
-def printTruthTable():
-    table = createTruthTable()
-    for key in table:
-        print(str(key) + ' : ' + str(table[key]))
-
-printTruthTable()
-
-def find_major_clause():
+def get_major_clause_tests(table):
     clauses = ['p', 'h', 'u', 'g']
-    table = createTruthTable()
-
     cPerRow = {}
     for elem in clauses:
         cPerRow[elem] = set()
 
     for cIdx, clause in enumerate(clauses):
-        # foundPair = False
-        for row in table.values():
+        for i, row in enumerate(table.values()):
             tmpRow = row.copy()
             tmpRow[cIdx] = not tmpRow[cIdx] # inverse le input de la clause majeure
             tmpRow[4] = not tmpRow[4]       # inverse le output du résultat
-            for nRow in table.values():
-                # si les clauses mineures sont égales pour un résultat et une clause majeure différents, RACC est respecté
+            for j, nRow in enumerate(table.values()):
                 if nRow == tmpRow:
-                    cPerRow[clause].add(tuple(row))
-                    cPerRow[clause].add(tuple(nRow))
+                    cPerRow[clause].add(i)
+                    cPerRow[clause].add(j)
                     break
     return cPerRow
 
-def print_valid_tests(tests):
-    for key in tests:
-        print(str(key) + ' : ')
-        tList = tests[key]
-        for row in tList:
-            print(str(row))
-
-tests = find_major_clause()
-print_valid_tests(tests)
-
-def find_cacc(tests):
+def apply_cacc(tests, table):
+    pair_found = False
+    retained_tests = {}
     for idx, key in enumerate(tests):
         tList = tests[key]
-        for row in tList:
+        for i in tList:
+            fRow = table[i].copy()
+            nC = not fRow[idx]
+            nP = not fRow[4]
+            for j in tList:
+                sRow = table[j]
+                if((nC == sRow[idx]) & (nP == sRow[4])):
+                    retained_tests[key] = set()
+                    retained_tests[key].add(i)
+                    retained_tests[key].add(j)
+                    pair_found = True
+                    break
+            if(pair_found):
+                pair_found = False
+                break      
+
+    return retained_tests
+
+def p_Table(table):
+    for key in table:
+        print(str(key) + ' : ' + str(table[key]))
+    print('\n')
+
+def p_rows(tests):
+    for key in tests:
+        output = str(key) + ' => '
+        tList = tests[key]
+        for idx, row in enumerate(tList):
+            output += 'd' + str(row)
+            if(idx < (len(tList) - 1)):
+                output += ', '
+        print(output)
+    print('\n')
+
+
+table = getTable()
+p_Table(table)
+
+################### CACC ######################
+
+tests = get_major_clause_tests(table)
+valid_pairs = apply_cacc(tests, table)
+print('CACC : ')
+p_rows(valid_pairs)
+
+
+################### GICC ######################
+
+def getGICC():
+    clauses = ['p', 'h', 'u', 'g']
+    cPerRow = {}
+    for elem in clauses:
+        cPerRow[elem] = set()
+
+    for cIdx, clause in enumerate(clauses):
+        for i, row in enumerate(table.values()):
             tmpRow = row.copy()
-            nC = not tmpRow[idx]
-            nP = not tmpRow[4]
-            for nRow in tList:
-                if((nC == nRow[idx]) & (nP == nRow[4])):
-                    
+            tmpRow[cIdx] = not tmpRow[cIdx] 
+            if(mapcontaine(tmpro[4]))
+            else:
+                for j, nRow in enumerate(table.values()):
+                    if nRow == tmpRow:
+                        cPerRow[clause].add(i)
+                        cPerRow[clause].add(j)
+                        mapPush(false)
+                        break
+    return cPerRow
